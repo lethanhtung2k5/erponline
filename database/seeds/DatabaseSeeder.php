15117a9422b4f7f13f3195f4e5a4679ebe735251
ethanhtung2k5 <lethanhtung2k5@gmail.com>
2019-01-01 15:54:42 +0700

    add user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        
+        DB::table('users)->insert([
+            ['fullname' => 'Lê Thanh Tùng'],
+            ['password' => bcrypt(123456)],
+            ['email' => 'lethanhtung2k5@gmail.com'],
+        ]);
     }
 }
